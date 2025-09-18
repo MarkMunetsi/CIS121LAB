7d7c927635341ac2e21d5abda4982dc0d1c7c569
MarkMunetsi <kundaimunetsi12@gmail.com>
2025-09-18 12:24:07 -0500

    question 3 done

--- labs/week4_p1.py
+++ labs/week4_p1.py
@@ -1,5 +1,5 @@
 #Mark Munetsi
-
+'''
 larger_num = int(input("enter the larger number: "))
 smaller_num = int(input("enter the smaller number"))
 
@@ -10,4 +10,22 @@ while larger_num > smaller_num:
     num += 1
 
 print(f"number of times halved: {num}")    
+
+
+#question 2
+
+word = input("enter a word: ")
+result = ""
+
+for i in range (1, len(word)-1,2):
+    result += word[i]
+
+print(f"output = {result}")    
+'''
+#question 3
+
+for num in range(37,1051):
+    if num % 2 == 0:
+    
+     print(num)
 
